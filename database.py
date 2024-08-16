c552073463d77052959187f6859d2dfc3b7f3998
akaprajapati <92862436+akaprajapati@users.noreply.github.com>
2024-08-16 20:08:56 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,9 @@ from models import Base
 
 DATABASE_URL = "postgresql://smartpot_user:gia@localhost:5432/smartpot_db"
 
+if not DATABASE_URL:
+    raise ValueError("No DATABASE_URL environment variable found.")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
