399fbf392cb7cb867abb81fdb89b8a3ddbd171df
akaprajapati <92862436+akaprajapati@users.noreply.github.com>
2024-08-17 19:00:35 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+from .config import settings
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 # Create the SQLAlchemy engine using the database URL from the settings
-engine = create_engine(settings.database_url)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
